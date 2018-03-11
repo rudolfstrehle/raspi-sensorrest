5508a0f0cc0d66742d418570322ee9c6d24eff93
rs <rs@kn-rstrehle.kisters.de>
2018-03-11 13:40:55 +0100

    Py 3.5 compatibility

--- rs/raspi/sensorrest/restapi.py
+++ rs/raspi/sensorrest/restapi.py
@@ -1,30 +1,30 @@
-from flask_restplus import Resource, Api, fields
-from flask_restplus.namespace import Namespace
+import flask_restplus
 from werkzeug.exceptions import NotFound
 
 from .sensorinfo import SensorApi
 
 
-def init(api: Api, sensors: SensorApi):
-    ns = Namespace(name="Sensors", path="/rest/sensors",
-                   description="Sensors REST API")
+def init(api: flask_restplus.Api, sensors: SensorApi):
+    ns = flask_restplus.namespace.Namespace(name="Sensors",
+                                            path="/rest/sensors",
+                                            description="Sensors REST API")
 
     sensorInfo = ns.model('SensorInfo', {
-        'sensor_id': fields.String,
-        'name': fields.String,
-        'value': fields.Float,
-        'when': fields.DateTime,
-        'unit': fields.String
+        'sensor_id': flask_restplus.fields.String,
+        'name': flask_restplus.fields.String,
+        'value': flask_restplus.fields.Float,
+        'when': flask_restplus.fields.DateTime,
+        'unit': flask_restplus.fields.String
     })
 
     @ns.route('/')
-    class Sensors(Resource):
+    class Sensors(flask_restplus.Resource):
         @ns.marshal_list_with(sensorInfo)
         def get(self):
             return list(sensors)
 
     @ns.route('/<string:sensor_id>')
-    class Sensor(Resource):
+    class Sensor(flask_restplus.Resource):
         @ns.marshal_with(sensorInfo)
         @ns.response(404, "If sensor id is not known")
         def get(self, sensor_id):
--- rs/raspi/sensorrest/runner.py
+++ rs/raspi/sensorrest/runner.py
@@ -1,13 +1,16 @@
 from rs.raspi.sensorrest.restapi import init as rest
 from flask.app import Flask
 from flask_restplus.api import Api
-from rs.raspi.sensorrest.dummysensors import DummySensors
-from rs.raspi.sensorrest.w1sensors import W1Sensors
+
+try:
+    from rs.raspi.sensorrest.w1sensors import W1Sensors
+    sensors = W1Sensors()
+except:  # @IgnorePep8
+    from rs.raspi.sensorrest.dummysensors import DummySensors
+    sensors = DummySensors()
 
 if __name__ == '__main__':
     app = Flask(__name__)
     api = Api(app, title="Raspberri Pi Sensor REST API")
-#    sensors = DummySensors()
-    sensors = W1Sensors()
     rest(api, sensors)
     app.run(debug=True)
